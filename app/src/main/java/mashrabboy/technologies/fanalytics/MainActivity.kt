ec067ca3c2b97da3f2aba8ba6ef3e7c7d1182425
Doniyorbek <doniyorbek.mashrabboyev@gmail.com>
2019-09-23 15:33:44 +0500

    first commit

---
+++ app/src/main/java/mashrabboy/technologies/fanalytics/MainActivity.kt
@@ -0,0 +1,12 @@
+package mashrabboy.technologies.fanalytics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
