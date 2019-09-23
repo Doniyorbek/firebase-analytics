a391f23bc9965a68e6ecc6e124006f86bd6f0161
Doniyorbek <doniyorbek.mashrabboyev@gmail.com>
2019-09-23 16:18:11 +0500

    added a event and fore crash button

--- app/src/main/java/mashrabboy/technologies/fanalytics/MainActivity.kt
+++ app/src/main/java/mashrabboy/technologies/fanalytics/MainActivity.kt
@@ -1,12 +1,30 @@
 package mashrabboy.technologies.fanalytics
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import com.google.firebase.analytics.FirebaseAnalytics
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+            Crashlytics.getInstance().crash()
+        }
+
+        btnSend.setOnClickListener {
+            val bundle = Bundle()
+            val firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+            firebaseAnalytics.setAnalyticsCollectionEnabled(true)
+            bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "256")
+            bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Custom Statistic Name")
+            bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+            firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+        }
+
     }
 }
